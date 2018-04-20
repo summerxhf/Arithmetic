e6ff3ca870021b13615a58aaebc88dd8734487dc
xhaifang <xinghaifang@kingsoft.com>
2018-04-20 11:18:54 +0800

    获取矩阵中标记的区域块数量算法

---
+++ src/main/java/others/BlockCounter.java
@@ -0,0 +1,152 @@
+package others;
+/**
+ * 数组区块数量判断:
+ * 1.给出一个二维数组,数组内容值为(0或1);
+ * 2.值为1的连续区块识别为一块,单独的独立区块也识别为一块;
+ * 3.判断得到的块的数量(类似于地图判断房屋数量).
+ * @author xhf
+ */
+public class BlockCounter {
+    private static final int SOMETHING = 1;
+    private static final int NOTHING = 0;
+
+    private int[][] grid;
+    private int rows;
+    private int cols;
+    private boolean defaultReset = false;
+
+    public BlockCounter(int[][] arr) {
+        grid = arr;
+        rows = grid.length;
+        cols = grid[0].length;
+    }
+
+    public boolean hasSomething(int i, int j) {
+        return grid[i][j] == SOMETHING;
+    }
+
+    public boolean isAlone(int i, int j) {
+        if (i == 0 && j == 0) {
+            return grid[i + 1][j] == NOTHING && grid[i][j + 1] == NOTHING;
+        } else if (i == 0 && j == cols - 1) {
+            return grid[i + 1][j] == NOTHING && grid[i][j - 1] == NOTHING;
+        } else if (i == rows - 1 && j == 0) {
+            return grid[i - 1][j] == NOTHING && grid[i][j + 1] == NOTHING;
+        } else if (i == rows - 1 && j == cols - 1) {
+            return grid[i - 1][j] == NOTHING && grid[i][j - 1] == NOTHING;
+        } else if (i == 0) {
+            return grid[i + 1][j] == NOTHING && grid[i][j - 1] == NOTHING && grid[i][j + 1] == NOTHING;
+        } else if (i == rows - 1) {
+            return grid[i - 1][j] == NOTHING && grid[i][j - 1] == NOTHING && grid[i][j + 1] == NOTHING;
+        } else if (j == 0) {
+            return grid[i - 1][j] == NOTHING && grid[i + 1][j] == NOTHING && grid[i][j + 1] == NOTHING;
+        } else if (j == cols - 1) {
+            return grid[i - 1][j] == NOTHING && grid[i + 1][j] == NOTHING && grid[i][j - 1] == NOTHING;
+        } else {
+            return grid[i - 1][j] == NOTHING && grid[i + 1][j] == NOTHING && grid[i][j - 1] == NOTHING
+                    && grid[i][j + 1] == NOTHING;
+        }
+    }
+
+    private void mark(int i, int j, int identify) {
+        if (hasSomething(i, j)) {
+            grid[i][j] = identify;
+        } else {
+            return;
+        }
+        if (i == 0 && j == 0) {
+            mark(i + 1, j, identify);
+            mark(i, j + 1, identify);
+        } else if (i == 0 && j == cols - 1) {
+            mark(i + 1, j, identify);
+            mark(i, j - 1, identify);
+        } else if (i == rows - 1 && j == 0) {
+            mark(i - 1, j, identify);
+            mark(i, j + 1, identify);
+        } else if (i == rows - 1 && j == cols - 1) {
+            mark(i - 1, j, identify);
+            mark(i, j - 1, identify);
+        } else if (i == 0) {
+            mark(i + 1, j, identify);
+            mark(i, j - 1, identify);
+            mark(i, j + 1, identify);
+        } else if (i == rows - 1) {
+            mark(i - 1, j, identify);
+            mark(i, j - 1, identify);
+            mark(i, j + 1, identify);
+        } else if (j == 0) {
+            mark(i - 1, j, identify);
+            mark(i + 1, j, identify);
+            mark(i, j + 1, identify);
+        } else if (j == rows - 1) {
+            mark(i - 1, j, identify);
+            mark(i + 1, j, identify);
+            mark(i, j - 1, identify);
+        } else {
+            mark(i - 1, j, identify);
+            mark(i + 1, j, identify);
+            mark(i, j - 1, identify);
+            mark(i, j + 1, identify);
+        }
+    }
+
+    private int countAloneCells() {
+        int count = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (hasSomething(i, j) && isAlone(i, j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private int countTogetherCells() {
+        reset();
+        int count = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (hasSomething(i, j) && !isAlone(i, j)) {
+                    mark(i, j, count + 2);// 对已经判断过的区块进行标识,并且不同区块标识数不同
+                    count++;
+                }
+            }
+        }
+        if (defaultReset) {
+            reset();
+        }
+        return count;
+    }
+
+    public int count() {
+        return countAloneCells() + countTogetherCells();
+    }
+
+    public void setDefaultReset(boolean defaultReset) {
+        this.defaultReset = defaultReset;
+    }
+
+    public void reset() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] != 0) {
+                    grid[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    public int[][] getGrid() {
+        return grid;
+    }
+
+    public void showGrid() {
+        for (int[] is : grid) {
+            for (int i : is) {
+                System.out.print(i + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/others/BlockCounterTest.java
@@ -0,0 +1,30 @@
+package others;
+
+import java.util.Random;
+
+public class BlockCounterTest {
+    // 生成一个随机的二维数组
+    public static int[][] createRandomArr(int rows, int cols) {
+        Random random = new Random();
+        int[][] arr = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                float n = random.nextFloat();
+                arr[i][j] = Math.round(n);
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[][] testArr = createRandomArr(6, 6);
+        BlockCounter blockCounter = new BlockCounter(testArr);
+        blockCounter.setDefaultReset(false);// 此语句影响grid数组内容,如非默认重置则需要手动重置,必须在count执行前设置
+        System.out.println("原数组:");
+        blockCounter.showGrid();
+        System.out.println("区块数:" + blockCounter.count());
+        System.out.println("处理后数组:");
+//        // blockCounter.reset();
+        blockCounter.showGrid();
+    }
+}
