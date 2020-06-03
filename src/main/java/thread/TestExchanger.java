74511db6d0aaf5ac03d2dc197197f951b6f8f463
xinghf <xinghaifang@kingsoft.com>
2020-06-03 19:03:51 +0800

    add exchanger 两个线程交换数据

---
+++ src/main/java/thread/TestExchanger.java
@@ -0,0 +1,40 @@
+package thread;
+
+import java.util.concurrent.Exchanger;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: XINGHAIFANG
+ * Date: 2020/6/3
+ * Time: 18:05
+ */
+public class TestExchanger {
+    static Exchanger<String> exchanger = new Exchanger<>();
+
+    public static void main(String[] args) {
+        new Thread(()->{
+            String s = "T1";
+            try {
+                s = exchanger.exchange(s);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " " + s);
+
+        }, "t1").start();
+
+
+        new Thread(()->{
+            String s = "T2";
+            try {
+                s = exchanger.exchange(s);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " " + s);
+
+        }, "t2").start();
+
+
+    }
+}
