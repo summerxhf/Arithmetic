ed75611db960f2bae01c7f79d01ed1e8470a6bb5
xinghf <xinghaifang@kingsoft.com>
2020-06-03 10:49:11 +0800

    add lock interrupt

---
+++ src/main/java/thread/InterruptLock.java
@@ -0,0 +1,53 @@
+package thread;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: XINGHAIFANG
+ * Date: 2020/6/3
+ * Time: 10:41
+ */
+public class InterruptLock {
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+        Thread t1 = new Thread(()->{
+            try{
+                lock.lock();
+                System.out.println("t1 start");
+                TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
+                System.out.println("t1 end");
+            }catch (InterruptedException e){
+                System.out.println();
+            }finally {
+                lock.unlock();
+            }
+        });
+        t1.start();
+
+        Thread t2 =new Thread(()->{
+            try{
+//                lock.lock();
+                lock.lockInterruptibly();;
+                System.out.println("t2 start");
+                TimeUnit.SECONDS.sleep(5);
+                System.out.println("t2 end");
+            }catch (InterruptedException e){
+                e.printStackTrace();
+                System.out.println("interrupted!");
+            }finally {
+                lock.unlock();
+            }
+        });
+        t2.start();
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t2.interrupt(); //打断线程2的等待
+    }
+}
