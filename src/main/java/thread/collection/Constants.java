4c6da49de6a0b104d3b4782d0aec455e7e4f7996
xinghf <xinghaifang@kingsoft.com>
2020-06-11 10:12:51 +0800

    add hashMap hashtable synchronizedHashMap ConcurentHashMap 比较

---
+++ src/main/java/thread/collection/Constants.java
@@ -0,0 +1,12 @@
+package thread.collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: XINGHAIFANG
+ * Date: 2020/6/10
+ * Time: 19:36
+ */
+public class Constants {
+    public static final int COUNT = 1000000;
+    public static final int THREAD_COUNT = 100;
+}
