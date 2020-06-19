c90380e8a3e0976b55e4b1762fa47a8c1ddda618
xinghf <xinghaifang@kingsoft.com>
2020-06-19 15:35:29 +0800

    add ScheduledPool 定时任务

---
+++ src/main/java/thread/threadpool/ScheduledPoolTest.java
@@ -0,0 +1,27 @@
+package thread.threadpool;
+
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: XINGHAIFANG
+ * Date: 2020/6/19
+ * Time: 15:31
+ * 定时任务使用;
+ */
+public class ScheduledPoolTest {
+    public static void main(String[] args) {
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(4);
+        service.scheduleAtFixedRate(()->{
+            try {
+                TimeUnit.MILLISECONDS.sleep(new Random().nextInt(1000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName());
+        },0,500, TimeUnit.MILLISECONDS);
+    }
+}
