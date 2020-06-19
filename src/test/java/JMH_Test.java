f5896dc3b57aaeb60a40efb92d87439d75f13ece
xinghf <xinghaifang@kingsoft.com>
2020-06-19 18:22:23 +0800

    add JMH

---
+++ src/test/java/JMH_Test.java
@@ -0,0 +1,16 @@
+package thread.JMH;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: XINGHAIFANG
+ * Date: 2020/6/19
+ * Time: 18:12
+ */
+public class JMH_Test {
+    @Benchmark
+    public void testForEach(){
+        PS.foreach();
+    }
+}
